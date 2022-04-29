db2d75dc96337260e4a9be03db867263985eb8ed
PabloN1 <nunong1995@gmail.com>
2022-04-29 08:18:45 +0200

    dia 28 14:30

--- Calculadora/js/script.js
+++ Calculadora/js/script.js
@@ -7,25 +7,32 @@ let division=0
 
 function aritmetico(operador){
     var expre
+    num1=Number(document.getElementById("Expresion").value)
+    console.log(num1)
     switch(operador){
         case 1:
-            expre=document.getElementById("Expresion").value+"+"
+            expre=document.getElementById("Expresion").value+" + "
+            suma=suma+1
             break;
         case 2:
-            expre=document.getElementById("Expresion").value+"-"
+            expre=document.getElementById("Expresion").value+" - "
+            resta=resta+1
             break;
         case 3:
-            expre=document.getElementById("Expresion").value+"*"
+            expre=document.getElementById("Expresion").value+" * "
+            multiplicacion=multiplicacion+1
             break;
         case 4:
-            expre=document.getElementById("Expresion").value+"/"
+            expre=document.getElementById("Expresion").value+" / "
+            division=division+1
+            break;
     }
     
    
     num1=document.getElementById("Expresion").value
     console.log(expre)
     document.getElementById("Expresion").value=expre
-    suma=suma+1
+    
 
     
 }
@@ -38,6 +45,20 @@ function numero(num){
 }
 
 
+function borrar(){
+    document.getElementById("Expresion").value=""
+    num1=0
+    num2=0
+    suma=0
+    resta=0
+    multiplicacion=0
+    division=0
+}
 
-
+function igual(){
+    var expre=(document.getElementById("Expresion").value).split(' ')
+    console.log(expre[0])
+    console.log(expre[1])
+    console.log(expre[2])
+}
 
