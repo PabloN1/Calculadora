6b57dd68ff40cd07e1290ee6263a67d0fcb57c0e
PabloN1 <nunong1995@gmail.com>
2022-04-29 13:28:09 +0200

    Finalizada parte JS, falta un poco mas de CSS

--- Calculadora/js/script.js
+++ Calculadora/js/script.js
@@ -9,31 +9,35 @@ function aritmetico(operador){
     var expre
     num1=Number(document.getElementById("Expresion").value)
     console.log(num1)
-    switch(operador){
-        case 1:
-            expre=document.getElementById("Expresion").value+" + "
-            suma=suma+1
-            break;
-        case 2:
-            expre=document.getElementById("Expresion").value+" - "
-            resta=resta+1
-            break;
-        case 3:
-            expre=document.getElementById("Expresion").value+" * "
-            multiplicacion=multiplicacion+1
-            break;
-        case 4:
-            expre=document.getElementById("Expresion").value+" / "
-            division=division+1
-            break;
-    }
-    
-   
-    num1=document.getElementById("Expresion").value
-    console.log(expre)
-    document.getElementById("Expresion").value=expre
-    
+    if(num1==""){
+        if(operador==2){
+            expre=document.getElementById("Expresion").value+"-"
+        }
+    }else{
+        switch(operador){
+            case 1:
+                expre=document.getElementById("Expresion").value+" + "
+                suma=suma+1
+                break;
+            case 2:
+                expre=document.getElementById("Expresion").value+" - "
+                resta=resta+1
+                break;
+            case 3:
+                expre=document.getElementById("Expresion").value+" * "
+                multiplicacion=multiplicacion+1
+                break;
+            case 4:
+                expre=document.getElementById("Expresion").value+" / "
+                division=division+1
+                break;
+        }
+        num1=document.getElementById("Expresion").value
+        
 
+    }
+    console.log(expre)
+        document.getElementById("Expresion").value=expre
     
 }
 
@@ -44,6 +48,10 @@ function numero(num){
     document.getElementById("Expresion").value=expre
 }
 
+function coma(){
+    var expre=document.getElementById("Expresion").value+"."
+    document.getElementById("Expresion").value=expre
+}
 
 function borrar(){
     document.getElementById("Expresion").value=""
@@ -60,5 +68,20 @@ function igual(){
     console.log(expre[0])
     console.log(expre[1])
     console.log(expre[2])
+    num2=Number(expre[2])
+    if(suma>0){
+        document.getElementById("Expresion").value=Number(num1)+Number(num2)
+    }else if(resta>0){
+        document.getElementById("Expresion").value=Number(num1)-Number(num2)
+    }else if(multiplicacion>0){
+        document.getElementById("Expresion").value=Number(num1)*Number(num2)
+    }else if(division>0){
+        document.getElementById("Expresion").value=Number(num1)/Number(num2)
+    }
+    
+    suma=0
+    resta=0
+    multiplicacion=0
+    division=0
 }
 
